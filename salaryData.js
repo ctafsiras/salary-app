2e4ac64f179993a77d30eb666c6313fa056b2444
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2022-03-06 23:05:11 +0600

    hoice

--- salaryData.js
+++ salaryData.js
@@ -1,64 +1,64 @@
 let salary_data = {
   0: {
     hobby: "talking javascript",
-    name: "Addy Osmani",
-    salary: "76340.0",
+    name: "Ertugrul Gazi",
+    salary: "72340.0",
   },
   1: {
-    name: "Paul Irish",
-    salary: "94320.0",
+    name: "Afridi",
+    salary: "92320.0",
     status: "married",
   },
   2: {
-    name: "Pete Hunt",
-    salary: "67000.0",
+    name: "Harry Potter",
+    salary: "67320.0",
   },
   3: {
-    name: "Pete sampras",
-    salary: "105540.0",
+    name: "MS Dhoni",
+    salary: "23540.0",
   },
   4: {
-    name: "Remy Sharp",
-    salary: "82220.0",
+    name: "Dinesh Kartik",
+    salary: "45220.0",
   },
   5: {
-    name: "David Beckham",
+    name: "Shakib Al Hasan",
     salary: "64430.0",
     sleepHour: 4,
   },
   6: {
     favLibrary: "React",
-    name: "A Random Guy",
+    name: "Kolim Uddin",
     pizzaSlice: 5,
-    salary: "36000.0",
+    salary: "36300.0",
   },
   7: {
     name: "JS Dude",
     salary: "44432",
   },
   "-JvacZOcfUEvub7mxeo-": {
-    name: "Jhankar Mahbub",
+    name: "Chy Tafsir Ahmed S",
     organization: "Nielsen",
-    salary: "53454",
+    salary: "53254",
   },
   "-Jvac_s61TL9oYM5SjMR": {
-    name: "Jhankar Mahbub",
-    salary: "43454",
+    name: "Chy Tafsir Ahmed S",
+    salary: "43354",
   },
   "-JvacbMTxlHCG5VaVDWg": {
-    name: "Jhankar Mahbub",
+    name: "Chy Tafsir Ahmed S",
     salary: "33454",
   },
   "-JviNyklLgZqk4rSB8pA": {
-    name: "Jhankar Mahbub",
+    name: "Chy Tafsir Ahmed S",
     salary: "23444",
   },
   "-JviO0Y_tNNMQNrdZVBR": {
-    name: "Jhankar Mahbub",
+    name: "Chy Tafsir Ahmed S",
     salary: "63444",
   },
   "-JviO3gXUpSdnZHtliWg": {
-    name: "Jhankar Mahbub",
+    name: "Chy Tafsir Ahmed S",
     salary: "53655",
   },
 };
